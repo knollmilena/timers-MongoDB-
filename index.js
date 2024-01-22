2700561b62c133abbd84ecce50bf48453d05ea1e
knollmilena <50546237+knollmilena@users.noreply.github.com>
2024-01-22 13:29:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,190 @@
+require("dotenv").config();
+
+const express = require("express");
+const nunjucks = require("nunjucks");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { nanoid } = require("nanoid");
+const bcrypt = require("bcryptjs");
+
+const app = express();
+
+const { MongoClient, ObjectId } = require("mongodb");
+
+const clientPromise = MongoClient.connect(process.env.DB_URL, {
+  userUnifiedTopology: true,
+  poolSize: 10,
+});
+
+const router = express.Router();
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  tags: {
+    blockStart: "[%",
+    blockEnd: "%]",
+    variableStart: "[[",
+    variableEnd: "]]",
+    commentStart: "[#",
+    commentEnd: "#]",
+  },
+});
+
+app.set("view engine", "njk");
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(async (req, res, next) => {
+  try {
+    const client = await clientPromise;
+    req.db = client.db("users");
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+const auth = () => async (req, res, next) => {
+  if (!req.cookies["sessionId"]) {
+    return next();
+  }
+  const user = await findUserBySessionId(req.db, req.cookies["sessionId"]);
+  req.user = user;
+  req.sessionId = req.cookies["sessionId"];
+  next();
+};
+
+app.get("/", auth(), (req, res) => {
+  res.render("index", {
+    user: req.user,
+    authError: req.query.authError === "true",
+  });
+});
+
+const createUser = async (db, username, password) => {
+  const id = ObjectId();
+  password = bcrypt.hashSync(password, 7);
+  await db.collection("users").insertOne({ _id: ObjectId(id), username, password });
+  return id;
+};
+
+const findUserByUsername = async (db, username) => db.collection("users").findOne({ username });
+
+const findUserBySessionId = async (db, sessionId) => {
+  const session = await db.collection("sessions").findOne(
+    { sessionId },
+    {
+      projection: { userId: 1 },
+    }
+  );
+
+  if (!session) {
+    return;
+  }
+  return db.collection("users").findOne({ _id: ObjectId(session.userId) });
+};
+
+const createSession = async (db, userId) => {
+  const sessionId = nanoid();
+
+  await db.collection("sessions").insertOne({
+    userId,
+    sessionId,
+  });
+
+  return sessionId;
+};
+
+async function deleteSession(db, sessionId) {
+  await db.collection("sessions").deleteOne({ sessionId });
+}
+
+app.post("/login", bodyParser.urlencoded({ extended: false }), async (req, res) => {
+  const { username, password } = req.body;
+  const user = await findUserByUsername(req.db, username);
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    return res.redirect("/?authError=true");
+  }
+  const sessionId = await createSession(req.db, user._id);
+  res.cookie("sessionId", sessionId, { httpOnly: true }).redirect("/");
+});
+
+app.post("/signup", bodyParser.urlencoded({ extended: false }), async (req, res) => {
+  const { username, password } = req.body;
+  const user = await findUserByUsername(req.db, username);
+  // console.log(bcrypt.compareSync(password, user.password));
+  if (user && bcrypt.compareSync(password, user.password)) {
+    console.log("Такой пользователь уже существует");
+    const sessionId = await createSession(req.db, user._id);
+    res.cookie("sessionId", sessionId, { httpOnly: true }).redirect("/");
+  } else if (user && !bcrypt.compareSync(password, user.password)) {
+    return res.redirect("/?authError=true");
+  } else {
+    const idUser = await createUser(req.db, username, password);
+    const sessionId = await createSession(req.db, idUser);
+    res.cookie("sessionId", sessionId, { httpOnly: true }).redirect("/");
+  }
+});
+
+app.get("/logout", auth(), async (req, res) => {
+  if (!req.user) {
+    return res.redirect("/");
+  }
+  await deleteSession(req.db, req.sessionId);
+  res.clearCookie("sessionId").redirect("/");
+});
+
+function createInterval(timers) {
+  for (let i = 0; i < timers.length; i++) {
+    timers[i].progress = Date.now() - timers[i].start;
+  }
+  return timers;
+}
+
+app.get("/api/timers", auth(), async (req, res) => {
+  let data = await req.db.collection("timers").find({}).toArray();
+  data = createInterval(data);
+  // console.log(req.user._id);
+
+  if (req.query.isActive === "true") {
+    data = data.filter((u) => u.isActive === true && JSON.stringify(u.user_id) === JSON.stringify(req.user._id));
+    res.json(data);
+  } else {
+    data = data.filter((u) => u.isActive === false && JSON.stringify(u.user_id) === JSON.stringify(req.user._id));
+    res.json(data);
+  }
+});
+
+app.post("/api/timers", auth(), async (req, res) => {
+  const timer = {
+    user_id: req.user._id,
+    start: Date.now(),
+    description: `${req.body.description}`,
+    isActive: true,
+    progress: 0,
+  };
+  await req.db.collection("timers").insertOne(timer);
+  res.json(timer);
+});
+
+app.post("/api/timers/:id/stop", async (req, res) => {
+  // console.log(req.params.id);
+  const d = new Date();
+  const timer = await req.db.collection("timers").findOne({ _id: ObjectId(`${req.params.id}`) });
+  // console.log(timer);
+  const timer2 = await req.db
+    .collection("timers")
+    .updateOne(
+      { _id: ObjectId(`${req.params.id}`) },
+      { $set: { duration: Date.now() - timer.start, end: Date.now(), isActive: false } }
+    );
+  res.json(timer2);
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`  Listening on http://localhost:${port}`);
+});
